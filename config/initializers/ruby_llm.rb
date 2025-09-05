0cc41f0d65899324af6aa1cf7d372b152cbfa214
Josh McArthur <joshua.mcarthur@gmail.com>
2025-09-05 22:57:43 +1200

    Add newlines

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,6 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
+  config.openai_api_key = Rails.application.credentials.openai&.api_key || ENV["OPENAI_API_KEY"]
+  config.anthropic_api_key = Rails.application.credentials.anthropic&.api_key || ENV["ANTHROPIC_API_KEY"]
 
   # config.default_model = "gpt-4.1-nano"
 end
